b8514b89e69b5d5d54b1c57fb6c61e2b7ac03c3e
RUrzeda <robertourzeda@gmail.com>
2025-08-13 14:23:52 -0300

    teste para aula

--- bibliometric_analysis.py
+++ bibliometric_analysis.py
@@ -17,7 +17,7 @@ import re
 from collections import Counter
 import warnings
 warnings.filterwarnings('ignore')
-
+#teste total
 # Set style for matplotlib
 plt.style.use('seaborn-v0_8')
 sns.set_palette("husl")
